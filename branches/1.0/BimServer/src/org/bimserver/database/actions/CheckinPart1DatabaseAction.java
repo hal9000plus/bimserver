bb3e99543b6834e1a2352cd4fc271cefe35b13d0
ruben@logic-labs.nl <ruben@logic-labs.nl@22d71240-4879-e6bd-ac50-c68132091b5d>
2010-12-21 15:34:20 +0000

    http://code.google.com/p/bimserver/issues/detail?id=187

--- branches/1.0/BimServer/src/org/bimserver/database/actions/CheckinPart1DatabaseAction.java
+++ branches/1.0/BimServer/src/org/bimserver/database/actions/CheckinPart1DatabaseAction.java
@@ -14,6 +14,7 @@ import org.bimserver.database.store.log.AccessMethod;
 import org.bimserver.database.store.log.LogFactory;
 import org.bimserver.database.store.log.NewRevisionAdded;
 import org.bimserver.ifc.IfcModel;
+import org.bimserver.mail.MailSystem;
 import org.bimserver.rights.RightsManager;
 import org.bimserver.shared.UserException;
 
@@ -44,6 +45,9 @@ public class CheckinPart1DatabaseAction extends GenericCheckinDatabaseAction {
 		}
 		if (!RightsManager.hasRightsOnProjectOrSuperProjects(user, project)) {
 			throw new UserException("User has no rights to checkin models to this project");
+		}
+		if (!MailSystem.isValidEmailAddress(user.getUsername())) {
+			throw new UserException("Users must have a valid e-mail address to checkin");
 		}
 		checkCheckSum(project);
 		if (!project.getRevisions().isEmpty() && project.getRevisions().get(project.getRevisions().size()-1).getState() == CheckinState.STORING) {
